3c1c35c028b76b7db1994f3c081ea5c6028de399
Francesco Borg Bonello <50298612+itsciccio@users.noreply.github.com>
2024-10-23 16:21:13 +0200

    CreateAccreditationRequestDTO: userId: using NotBlank instead of NotNull

--- src/main/java/com/yieldstreet/takehome/user_accreditation/dto/request/CreateAccreditationRequestDTO.java
+++ src/main/java/com/yieldstreet/takehome/user_accreditation/dto/request/CreateAccreditationRequestDTO.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yieldstreet.takehome.user_accreditation.enums.AccreditationType;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateAccreditationRequestDTO {
     @JsonProperty("user_id") 
-    @NotNull(message = "User ID is required")
+    @NotBlank(message = "User ID is required")
     private String userId;
     
     @JsonProperty("accreditation_type") 
